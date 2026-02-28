3a72ea0176ca66832122b0101acb564cf0c4461c
Shaurya Jain <shauryajain19052003@gmail.com>
2026-02-28 18:48:37 +0530

    feat: add index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+// React Native index
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
