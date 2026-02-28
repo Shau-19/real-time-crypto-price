f94f6cc2861f7b2cb49772af7a50942324be29d1
Shaurya Jain <shauryajain19052003@gmail.com>
2026-02-28 18:48:36 +0530

    feat: add App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+// React Native app
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Real-time crypto price dashboard</Text>
+    </View>
+  );
+};
+
+export default App;
